1c0d51f11650f8658e84a595f83d72301f60dd69
Yasunori Ishiguro <ishiguro-y@dainichi-consul.co.jp>
2022-02-07 18:33:48 +0900

    温湿度センサーをクラス化

--- ambient-sensors.ino
+++ ambient-sensors.ino
@@ -1,21 +1,9 @@
 #include <Wire.h>
-#include "SparkFun_Si7021_Breakout_Library.h"
-#include "RPR-0521RS.h"
-
-#include "led.h"
 #include "serial_monitor.h"
 #include "lcd_display.h"
+#include "led.h"
+#include "temp_hum_sensor.h"
 #include "illuminance_sensor.h"
-
-// Si7021スレーブアドレス
-#define SI7021_ADDRESS 0x40
-
-// LED
-const int LED_PIN = 5;
-const int MAX_LED_VALUE = 100;
-const int LED_DELAY = 200;
-const int LED_STEP = 10;
-SwitchingLed led(LED_PIN, MAX_LED_VALUE, LED_STEP, LED_DELAY);
 
 // シリアルモニター
 #define SERIAL_BOARATE 9600
@@ -24,75 +12,40 @@ SerialMonitor monitor(SERIAL_BOARATE);
 // LCDディスプレイ
 LcdDisplay lcd;
 
-// Si7021センサーインスタンス
-Weather sensor;
+// LED
+const int LED_PIN = 5;
+const int MAX_LED_VALUE = 100;
+const int LED_DELAY = 200;
+const int LED_STEP = 10;
+SwitchingLed led(LED_PIN, MAX_LED_VALUE, LED_STEP, LED_DELAY);
+
+// 温湿度センサー
+#define SI7021_ADDRESS 0x40
+TempHumSensor temp_hum_sensor(SI7021_ADDRESS);
 
 // 照度センサー
 IlluminanceSensor ill_sensor;
 
-// 温度湿度構造体
-struct TempHum {
-  // 温度(℃)
-  float temp;
-  // 湿度(%)
-  float hum;
-};
-    // 華氏の温度を摂氏に変換する。
-    //
-    // Arguments:
-    //  f: 華氏。
-    //
-    // Returns:
-    //  摂氏。
-    float f_to_c(float f) {
-      return (f - 32.0) / 1.8;
-    }
-
-// Si7021で温度と湿度を計測した値を返却する。
-//
-// Arguments:
-//  pTempHum: 測定値した温度と湿度を記録する構造体のポインタ。
-void getTempHum(TempHum *pTempHum) {
-  pTempHum->temp = f_to_c(sensor.getTempF());
-  pTempHum->hum = sensor.getRH();
-}
-
 void setup() {
-  // LEDをの初期化
-  led.init();
   // シリアルモニタを初期化
   monitor.init();
+
   // LCDディスプレイを初期化
   lcd.init(7, 8, 9, 10, 11, 12);
 
+  // LEDをの初期化
+  led.init();
+
   // I2Cの利用を開始
   Wire.begin();
-  lcd.print_row(0, "I2C init.");
-  Serial.println("[INFO] I2C was initialized.");
 
-  // Si7021をリセット
-  Wire.beginTransmission(SI7021_ADDRESS);
-  Wire.write(0xFE); // リセットコマンドアドレス
-  Wire.endTransmission();
-  lcd.print_row(0, "Si7021 init.");
-  Serial.println("[INFO] Si7021 was initialized.");
-  delay(15);
-
-  // Si7021センサーの存在を確認
-  if (sensor.begin()) {
-    Serial.println("[INFO] Si7021 was detected.");
-    lcd.print_row(0, "Si7021 detected.");
-  } else {
-    Serial.println("[ERROR] Si7021 was not detected.");
-    lcd.print_row(0, "Si7021 fail.");
-    return;
-  }
+  // 温湿度センサーを初期化
+  temp_hum_sensor.init();
+  Serial.println("[INFO] Temp & hum sensor was initialized.");
+  lcd.print_row(0, "Temp-Hum init.");
 
   // 照度センサーを初期化
-  if (ill_sensor.init()) {
-    Serial.println("[INFO] Illuminance sensor was initialized.");
-    lcd.print_row(0, "RPR-0521RS init.");
-  } else {
+  if (!ill_sensor.init()) {
     Serial.println("[ERROR] Illuminance sensor could not initialized.");
     lcd.print_row(0, "Ill sensor fail.");
     return;
@@ -100,22 +53,23 @@ void setup() {
 
   // 測定を開始
   lcd.print_row(0, "Start measuring...");
-  delay(500);
+  delay(3000);
 }
 
 void loop() {
-  // 測定値
-  TempHum tempHum;
+  // 温湿度。
+  float temp, hum;
   // 照度。
   bool r_ill; float ill;
 
   // 温度と湿度を計測
-  getTempHum(&tempHum);
+  temp = temp_hum_sensor.measure_temp();
+  hum = temp_hum_sensor.measure_hum();
   // 照度を計測
   r_ill = ill_sensor.measure(&ill); 
   // 測定値を出力
-  monitor.print(tempHum.temp, tempHum.hum, ill);
-  lcd.print_measured_values(tempHum.temp, tempHum.hum, ill);
+  monitor.print(temp, hum, ill);
+  lcd.print_measured_values(temp, hum, ill);
 
   // LEDを点灯または消灯
   led.on_off();
---
+++ temp_hum_sensor.h
@@ -0,0 +1,62 @@
+#include "SparkFun_Si7021_Breakout_Library.h"
+
+class TempHumSensor {
+private:
+    // Si7021スレーブアドレス。
+    char _address;
+    // Si7021センサーインスタンス
+    Weather _sensor;
+
+    // デフォルトコンストラクタ
+    TempHumSensor(void) {}
+
+    // 華氏の温度を摂氏に変換する。
+    //
+    // Arguments:
+    //  f: 華氏。
+    //
+    // Returns:
+    //  摂氏。
+    float f_to_c(float f) {
+      return (f - 32.0) / 1.8;
+    }
+
+public:
+    // コンストラクタ
+    //
+    // Arguments:
+    //  address: Si7021スレーブアドレス。
+    TempHumSensor(char address) {
+        this->_address = address;
+    }
+
+    // 初期化する。
+    //
+    // Returns:
+    //  初期化に成功した場合はtrue。初期化に失敗した場合はfalse。
+    bool init() {
+        // Si7021をリセット
+        Wire.beginTransmission(this->_address);
+        Wire.write(0xFE); // リセットコマンドアドレス
+        Wire.endTransmission();
+        delay(15);
+        // Si7021センサーの存在を確認
+        return this->_sensor.begin();
+    }
+
+    // 温度を測定する。
+    //
+    // Returns:
+    //  温度。単位は度。
+    float measure_temp(void) {
+        return this->f_to_c(this->_sensor.getTempF());
+    }
+
+    // 湿度を測定する。
+    //
+    // Returns:
+    //  湿度。単位は%。
+    float measure_hum(void) {
+        return this->_sensor.getRH();
+    }
+};
