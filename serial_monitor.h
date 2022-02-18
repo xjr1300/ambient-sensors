ec515b1f98dcc6421df5712fc7ff07df25afffdf
Yasunori Ishiguro <ishiguro-y@dainichi-consul.co.jp>
2022-02-18 16:53:09 +0900

    シリアルモニタに土壌水分量を出力するように変更

--- serial_monitor.h
+++ serial_monitor.h
@@ -3,7 +3,7 @@ class SerialMonitor {
     // ボーレート
     int _boarate;
     // シリアルモニタに測定値を表示するときに使用するバッファ
-    char _serial[128], _temp[16], _hum[16], _ill[32];
+    char _serial[128], _temp[16], _hum[16], _ill[32], _moist[32];
 
     // デフォルトコンストラクタ。
     SerialMonitor(void) {}
@@ -31,12 +31,20 @@ class SerialMonitor {
     //  temp: 温度。
     //  hum: 湿度。
     //  ill: 照度。
-    void print_measured_values(float temp, float hum, float ill) {
+    //  moist: 土壌水分量。
+    void print_measured_values(float temp, float hum, float ill,
+                               int32_t moist) {
         dtostrf(temp, -1, 1, this->_temp);
         dtostrf(hum, -1, 1, this->_hum);
         dtostrf(ill, -1, 0, this->_ill);
-        sprintf(this->_serial, "Temp: %s[C], Hum: %s[%%], ALS: %s[lx]",
-                this->_temp, this->_hum, this->_ill);
+        if (0 <= moist) {
+            sprintf(this->_moist, "%d", moist);
+        } else {
+            strcpy(this->_moist, "Err.");
+        }
+        sprintf(this->_serial,
+                "Temp: %s[C], Hum: %s[%%], ALS: %s[lx], Moist: %d", this->_temp,
+                this->_hum, this->_ill, this->_moist);
         Serial.println(_serial);
     }
 };
