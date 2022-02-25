ba85d67db200156fcb692d160b62887a2e1a1f70
Yasunori Ishiguro <ishiguro-y@dainichi-consul.co.jp>
2022-02-25 10:30:04 +0900

    プログラムサイズの縮小を試行（Arduinoに書き込み不可）

--- ambient-sensors.ino
+++ ambient-sensors.ino
@@ -1,17 +1,25 @@
+#define __SERIAL_MONITOR__
+// #define __SD_CARD_LOGGER__
 #include <SPI.h>
 #include <Wire.h>
 
 #include "illuminance_sensor.h"
 #include "lcd_display.h"
 #include "led.h"
+#ifdef __SD_CARD_LOGGER__
 #include "sd_card_logger.h"
+#endif
+#ifdef __SERIAL_MONITOR__
 #include "serial_monitor.h"
+#endif
 #include "soil_moisture_sensor.h"
 #include "temp_hum_sensor.h"
 
+#ifdef __SERIAL_MONITOR__
 // シリアルモニター
 #define SERIAL_BOARATE 9600
 SerialMonitor serial_monitor(SERIAL_BOARATE);
+#endif
 
 // LCDディスプレイ
 LcdDisplay lcd_display;
@@ -33,10 +41,11 @@ IlluminanceSensor ill_sensor;
 // 土壌水分センサー
 SoilMoistureSensor moist_sensor(10, 10);
 
+#ifdef __SD_CARD_LOGGER__
 // SDカードロガー
 // 100万データ出力したらファイルをローテーション
-#define SD_FAT_TYPE 3
 SDCardLogger logger(SD_CARD_CHIP_UNO, 1000000, 10);
+#endif
 
 void setup(void) {
     // I2Cの利用を開始
@@ -44,8 +53,10 @@ void setup(void) {
     // SPIの利用を開始
     SPI.begin();
 
+#ifdef __SERIAL_MONITOR__
     // シリアルモニタを初期化
     serial_monitor.init();
+#endif
     // lcd_displayディスプレイを初期化
     // lcd_display.init(7, 8, 9, 10, 11, 12);
     lcd_display.init(7, 8, 9, 4, 5, 6);
@@ -55,25 +66,24 @@ void setup(void) {
     temp_hum_sensor.init();
     // 環境光照度センサーを初期化
     if (!ill_sensor.init()) {
-        Serial.println("Ill err.");
         lcd_display.print_row(0, "Ill err.");
         return;
     }
     // 土壌水分センサーを初期化
     if (!moist_sensor.init()) {
-        Serial.println("Moist err.");
         lcd_display.print_row(0, "Moist err.");
         return;
     }
+#ifdef __SD_CARD_LOGGER__
     // SDカードロガーを初期化
     if (!logger.init()) {
-        Serial.println("Card err.");
         lcd_display.print_row(0, "Card err.");
         return;
     }
+#endif
 
     // 測定を開始
-    lcd_display.print_row(0, "Start measuring...");
+    lcd_display.print_row(0, "Start...");
     delay(1000);
 }
 
@@ -81,7 +91,6 @@ void loop(void) {
     // 温湿度値
     float temp, hum;
     // 環境光照度値
-    bool r_als;
     float als;
     // 土壌水分量
     int16_t moist;
@@ -90,14 +99,18 @@ void loop(void) {
     temp = temp_hum_sensor.measure_temp();
     hum = temp_hum_sensor.measure_hum();
     // 環境光照度を測定
-    r_als = ill_sensor.measure(&als);
+    ill_sensor.measure(&als);
     // 土壌水分量を測定
     moist = moist_sensor.measure();
 
     // 測定値を出力
+#ifdef __SERIAL_MONITOR__
     serial_monitor.print_measured_values(temp, hum, als, moist);
+#endif
     lcd_display.print_measured_values(temp, hum, als, moist);
+#ifdef __SD_CARD_LOGGER__
     logger.write_measured_values(temp, hum, als, moist);
+#endif
 
     // LEDを点灯または消灯
     led.on_off();
--- soil_moisture_sensor.h
+++ soil_moisture_sensor.h
@@ -12,9 +12,9 @@ const byte ADDRESS = 0x28;
 class SoilMoistureSensor {
    private:
     // センサーからの応答を取得する試行回数。
-    int _times;
+    byte _times;
     // センサーから応答の取得する間隔ミリ秒
-    int _interval;
+    byte _interval;
 
     // デフォルトコンストラクタ
     SoilMoistureSensor(void) {}
@@ -24,7 +24,7 @@ class SoilMoistureSensor {
     // Arguments:
     //  times: センサーからの応答を取得する試行回数。
     //  interval: センサーから応答の取得する間隔ミリ秒
-    SoilMoistureSensor(int times, int interval) {
+    SoilMoistureSensor(byte times, byte interval) {
         this->_times = times;
         this->_interval = interval;
     }
@@ -34,7 +34,7 @@ class SoilMoistureSensor {
     // Returns:
     //  センサーが接続されていることを確認できた場合はtrue。確認できなかった場合はfalse。
     bool init(void) {
-        for (int i = 0; i < this->_times; ++i) {
+        for (byte i = 0; i < this->_times; ++i) {
             Wire.beginTransmission(ADDRESS);
             if (Wire.endTransmission() == 0) {
                 return true;
@@ -52,7 +52,7 @@ class SoilMoistureSensor {
     //
     // Returns:
     //  土壌水分量。マイナスの値が返却された場合は測定エラー。
-    int32_t measure(void) {
+    int16_t measure(void) {
         // 測定する命令を発効
         Wire.beginTransmission(ADDRESS);
         Wire.write(COMMAND_GET_VALUE);
@@ -62,7 +62,7 @@ class SoilMoistureSensor {
         Wire.requestFrom(ADDRESS, (uint8_t)0x02);
 
         // 送信された測定値を読み込み
-        for (int i = 0; i < this->_times; ++i) {
+        for (byte i = 0; i < this->_times; ++i) {
             while (Wire.available()) {
                 uint8_t adc_value_l = Wire.read();
                 uint8_t adc_value_h = Wire.read();
@@ -73,10 +73,10 @@ class SoilMoistureSensor {
                     adc_value = 1023;
                 }
                 adc_value = 1023 - adc_value;
-                return (int32_t)((double)adc_value / 1023 * 100);
+                return (int16_t)((float)adc_value / 1023 * 100);
             }
             delay(this->_interval);
         }
-        return ~(int32_t)0;
+        return ~(int16_t)0;
     }
 };
